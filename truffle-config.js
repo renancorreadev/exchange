2887237184348b1034e2cfdaada72bfc07d3efad
skyxcripto <producer@skynance.net>
2021-12-22 11:26:53 -0300

    First test for Token.sol

---
+++ truffle-config.js
@@ -0,0 +1,35 @@
+require('babel-register')
+require('babel-polyfill')
+require('dotenv').config()
+// const HDWalletProvider = require('@truffle/hdwallet-provider');
+//
+// const fs = require('fs');
+// const mnemonic = fs.readFileSync(".secret").toString().trim();
+
+module.exports = {
+  networks: {
+    development: {
+      host: '127.0.0.1',
+      port: 8545,
+      network_id: '*', // Match any network id
+    },
+  },
+
+  contracts_directory: './src/contracts/',
+  contracts_build_directory: './src/abis/',
+
+  // Set default mocha options here, use special reporters etc.
+  mocha: {
+    // timeout: 100000
+  },
+
+  // Configure your compilers
+  compilers: {
+    solc: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+}
